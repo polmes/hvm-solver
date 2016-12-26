53a092ed1e44d515b5e287bba3829633cf40ca42
Pol Mesalles <pol.developer1@gmail.com>
2016-12-26 02:50:00 +0100

    Remove now unused code

--- uvw.m
+++
@@ -1,7 +0,0 @@
-function v = uvw(pp,p1,p2)
-    r0 = p2 - p1;
-    r1 = pp - p1;
-    r2 = pp - p2;
-    r1xr2 = cross(r1,r2);
-    v = 1/(4*pi) * (r0' * (r1/norm(r1) - r2/norm(r2))) *  r1xr2 / (r1xr2' * r1xr2);
-end
